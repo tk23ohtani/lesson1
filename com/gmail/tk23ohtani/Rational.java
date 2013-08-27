1b0beceaa111c51e8a5060c4cb93d95c91bd3d54
tk23ohtani <tk23ohtani@gmail.com>
2013-08-27 19:57:12 +0900

    Ranking

--- com/gmail/tk23ohtani/Rational.java
+++ com/gmail/tk23ohtani/Rational.java
@@ -14,19 +14,15 @@ import javax.swing.border.EtchedBorder;
  */
 public class Rational extends JFrame {
 
-	JLabel[] tx = new JLabel[10];
+	private final static int DEPTH = 20;
+	JLabel[] tx = new JLabel[DEPTH];
 	JTextField text1, text2;
 	JButton btnInstall;
 
 	public Rational() {
 
-		JPanel pnlData = new JPanel();
-		pnlData.setLayout(new GridLayout(2, 1));
-		EtchedBorder border = new EtchedBorder(EtchedBorder.RAISED, Color.white, Color.gray);
-		pnlData.setBorder(border);
-
 		JPanel pnlInfomation = new JPanel();
-		pnlInfomation.setLayout(new GridLayout(10, 1));
+		pnlInfomation.setLayout(new GridLayout(DEPTH, 1));
 		for (int i=0; i<tx.length; i++) {
 			pnlInfomation.add(tx[i] = new JLabel());
 		}
@@ -38,7 +34,6 @@ public class Rational extends JFrame {
 		text2 = new JTextField("5", 2);
 		pnlButtons.add(text2);
 		btnInstall = new JButton("Calcurate");
-//		btnInstall.setEnabled(false);
 		btnInstall.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -50,24 +45,47 @@ public class Rational extends JFrame {
 					margin = margin / 10.0;
 				}
 
-				double	dval;
-				int		i, j, k=0, sta_val, stp_val;
-				for (j=2; j<30; j++)
+				double	dval, diff;
+				double [] diffs = new double[DEPTH];
+				for (int l=0; l<DEPTH; l++)
+				{
+					diffs[l] = 10.0;
+					tx[l].setText("");
+
+				}
+				int		i, j, sta_val, stp_val;
+				for (j=2; j<200; j++)
 				{
 					sta_val = (((int)target) - 1) * j;
 					stp_val = (((int)target) + 1) * j;
 					for (i=sta_val; i<=stp_val; i++)
 					{
 						dval = (double)i / (double)j;
-						if ((dval > target - margin) && (dval < target + margin))
+						diff = Math.abs(target - dval);
+						if (margin > diff)
 						{
-							String str;
-							str = i + "/" + j + "=" + dval + "(" + (target - dval) + ")";
-							tx[k++].setText(str);
+							for (int l=0; l<DEPTH; l++)
+							{
+								if (diffs[l] == diff)
+								{
+									break;
+								}
+								if (diffs[l] > diff)
+								{
+									for (int m=DEPTH-1; l<m; m--)
+									{
+										tx[m].setText(tx[m-1].getText());
+										diffs[m] = diffs[m-1]; 
+									}
+									diffs[l] = diff;
+									String str;
+									str = i + "/" + j + "=" + dval + "(" + diff + ")";
+									tx[l].setText(str);
+									break;
+								}
+							}
 						}
-						if (k >= 10) break;
 					}
-					if (k >= 10) break;
 				}
 			}
 		});
@@ -75,13 +93,12 @@ public class Rational extends JFrame {
 
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(pnlButtons, BorderLayout.NORTH);
-		getContentPane().add(pnlData, BorderLayout.CENTER);
 		getContentPane().add(pnlInfomation, BorderLayout.SOUTH);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Rational");
 		setLocationRelativeTo(null);
-		setSize(350, 250);
+		setSize(350, 380);
 		setVisible(true);
 
 	}
