fcce375cbf30b90d3420cae83ab8e33e089b1613
tk23ohtani <tk23ohtani@gmail.com>
2013-08-28 09:41:34 +0900

    Refactoring 1

--- com/gmail/tk23ohtani/Rational.java
+++ com/gmail/tk23ohtani/Rational.java
@@ -31,62 +31,19 @@ public class Rational extends JFrame {
 		pnlButtons.setLayout(new FlowLayout());
 		text1 = new JTextField("3.1415927", 10);
 		pnlButtons.add(text1);
-		text2 = new JTextField("5", 2);
+		text2 = new JTextField("3", 2);
 		pnlButtons.add(text2);
 		btnInstall = new JButton("Calcurate");
 		btnInstall.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
 				double target = Double.parseDouble(text1.getText());
 				int digit = Integer.parseInt(text2.getText());
 				double margin = 1.0;
 				while (digit-- != 0) {
 					margin = margin / 10.0;
 				}
-
-				double	dval, diff;
-				double [] diffs = new double[DEPTH];
-				for (int l=0; l<DEPTH; l++)
-				{
-					diffs[l] = 10.0;
-					tx[l].setText("");
-
-				}
-				int		i, j, sta_val, stp_val;
-				for (j=2; j<200; j++)
-				{
-					sta_val = (((int)target) - 1) * j;
-					stp_val = (((int)target) + 1) * j;
-					for (i=sta_val; i<=stp_val; i++)
-					{
-						dval = (double)i / (double)j;
-						diff = Math.abs(target - dval);
-						if (margin > diff)
-						{
-							for (int l=0; l<DEPTH; l++)
-							{
-								if (diffs[l] == diff)
-								{
-									break;
-								}
-								if (diffs[l] > diff)
-								{
-									for (int m=DEPTH-1; l<m; m--)
-									{
-										tx[m].setText(tx[m-1].getText());
-										diffs[m] = diffs[m-1]; 
-									}
-									diffs[l] = diff;
-									String str;
-									str = i + "/" + j + "=" + dval + "(" + diff + ")";
-									tx[l].setText(str);
-									break;
-								}
-							}
-						}
-					}
-				}
+				calc(target, margin);
 			}
 		});
 		pnlButtons.add(btnInstall);
@@ -101,6 +58,51 @@ public class Rational extends JFrame {
 		setSize(350, 380);
 		setVisible(true);
 
+	}
+
+	private void calc(double target, double margin) {
+		double	dval, diff;
+		double [] diffs = new double[DEPTH];
+		for (int l=0; l<DEPTH; l++)
+		{
+			diffs[l] = 10.0;
+			tx[l].setText("");
+
+		}
+		int		i, j, sta_val, stp_val;
+		for (j=2; j<200; j++)
+		{
+			sta_val = (((int)target) - 1) * j;
+			stp_val = (((int)target) + 1) * j;
+			for (i=sta_val; i<=stp_val; i++)
+			{
+				dval = (double)i / (double)j;
+				diff = Math.abs(target - dval);
+				if (margin > diff)
+				{
+					for (int l=0; l<DEPTH; l++)
+					{
+						if (diffs[l] == diff)
+						{
+							break;
+						}
+						if (diffs[l] > diff)
+						{
+							for (int m=DEPTH-1; l<m; m--)
+							{
+								tx[m].setText(tx[m-1].getText());
+								diffs[m] = diffs[m-1]; 
+							}
+							diffs[l] = diff;
+							String str;
+							str = i + "/" + j + "=" + dval + "(" + diff + ")";
+							tx[l].setText(str);
+							break;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	/**
